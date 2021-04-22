904f8ceda4366e786ff12f11b56bc08329681caa
Watchara Thongkam <artozaka46@artos-MBA.local>
2021-04-22 18:50:20 +0700

    first commit

---
+++ alternative-flavor.js
@@ -0,0 +1,6 @@
+module.exports.items = ['iMac', 'iPhone'];
+const person = {
+    name: 'Seira',
+}
+
+module.exports.singlePerson = person
---
+++ names.js
@@ -0,0 +1,8 @@
+const seira = 'Seira'
+const kakki = 'Kakki'
+const yuri = 'Yuri'
+
+//local
+const mayutan = 'Mayutan'
+
+module.exports = { seira, kakki, yuri }
